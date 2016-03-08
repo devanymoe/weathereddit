a17e7344ff31a15b57f00e12dfb0d42276a3602b
Devany Moe <devanymoe@gmail.com>
2016-03-08 15:04:43 -0800

    Implement weather icons

---
+++ js/reddit.js
@@ -0,0 +1,47 @@
+(function() {
+  var $userZip = 98125;
+  var $userSubReddit = "trollxchromosomes";
+  var subRedditURL = "https://www.reddit.com/r/" + $userSubReddit + ".json";
+  var postText = [];
+  $.ajax({
+    type: "GET",
+    dataType: "json",
+    url: subRedditURL
+  }).then(function(response) {
+    var count = 0;
+    for (var i = 0; i < response.data.children.length; i++) {
+      var post = response.data.children[i].data
+      if (post.stickied === false && count < 10) {
+        var $redditPost = $('<div class="post"><div class="postMain"><a class="thumbURL"><div class="thumbnail"><img></div></a><div class="postContainer"><a class="postURL"><div class="title"></div></a><div class="postDetails"><span class="url"></span><a class="permalink"><span class="comments"></span></a><span class="author">by <a class="user"></a></span></div></div></div></div>');
+        var $postDetails = $('')
+        $redditPost.find('.title').text(post.title);
+        $redditPost.find('.postURL').attr('href',post.url);
+        $redditPost.find('.postURL').attr('target','_blank');
+        $redditPost.find('.url').text('(' + post.domain + ')');
+        $redditPost.find('.comments').text(post.num_comments + ' comments');
+        $redditPost.find('.permalink').attr('href', 'https://www.reddit.com' + post.permalink)
+        $redditPost.find('.user').text(post.author);
+        $redditPost.find('.user').attr('href', 'https://www.reddit.com/user/' + post.author);
+        postText.push(post.selftext);
+        if (post.preview === undefined) {
+          $redditPost.find('img').attr('src','images/textopen.jpg');
+          $redditPost.find('img').addClass('textimage');
+          // $redditPost.find('.thumbURL').attr('href',post.url);
+          // $redditPost.find('.thumbURL').attr('target','_blank');
+        }
+        else {
+          $redditPost.find('img').attr('src',post.thumbnail);
+          $redditPost.find('.thumbURL').attr('href',post.url);
+          $redditPost.find('.thumbURL').attr('target','_blank');
+        }
+        $reddit.append($redditPost);
+        count += 1;
+      }
+    }
+  });
+
+  var $expandPost = $('.textimage');
+  $expandPost.on('click', function(){
+    console.log("check");
+  })
+})()
---
+++ js/weather.js
@@ -0,0 +1,61 @@
+(function (){
+  var zip = 98125;
+  var weatherURL = "http://api.wunderground.com/api/4fb43427a15866c7/forecast/q/" + zip + ".json";
+
+  $.ajax({
+    type: "GET",
+    dataType: "json",
+    url: weatherURL
+  }).then(function(response) {
+    var $forecast = response.forecast.txt_forecast.forecastday;
+    var $forecastToday = $('.forecastToday');
+    var $today = $('<i class="wi"></i><div class="today"></div>');
+    $today.find('.today').text($forecast[0].fcttext);
+    var $icon = $today.find('i');
+    var iconPhrase = $forecast[0].icon;
+    switch(iconPhrase) {
+      case "chanceflurries":
+      case "chancesleet":
+      case "chancesnow":
+      case "flurries":
+      case "sleet":
+      case "snow":
+        $icon.addClass('wi-day-snow-wind');
+        break;
+      case "chancerain":
+      case "rain":
+        $icon.addClass('wi-showers');
+        break;
+      case "chancestorms":
+      case "tstorms":
+        $icon.addClass('wi-storm-showers');
+        break;
+      case "cloudy":
+        $icon.addClass('wi-cloud');
+        break;
+      case "fog":
+        $icon.addClass('wi-fog');
+        break;
+      case "hazy":
+        $icon.addClass('wi-day-haze');
+        break;
+      case "mostlycloudy":
+        $icon.addClass('wi-cloudy');
+        break;
+      case "mostlysunny":
+        $icon.addClass('wi-day-sunny-overcast');
+        break;
+      case "partlycloudy":
+      case "partlysunny":
+        $icon.addClass('wi-day-cloudy');
+        break;
+      case "sunny":
+      case "clear":
+        $icon.addClass('wi-day-sunny');
+        break;
+      default:
+        $icon.addClass('wi-cloud');
+    }
+    $forecastToday.append($today);
+  })
+})();
